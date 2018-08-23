3c2f4528b1ba12e58e0c346dc18a80b6534b2caf
Arthur Lorenz <arthur@drunomics.com>
2018-08-23 22:32:41 +0200

    Updated gallery.

--- README.md
+++ README.md
@@ -16,3 +16,20 @@ import { PgGallery } from 'vue-lupus-paragraph-gallery';
 
 Vue.component('pg-gallery', PgGallery);
 ```
+
+## Properties
+You can pass the following props:
+
+- `data-name` ( string )
+  The gallery's name.
+- `data-images` ( string )
+  JSON representation of the image array.
+    - [{'src': '/path/to/image', 'alt': 'The alt text'}]
+
+## Example
+```
+<pg-gallery
+  data-name="Quote"
+  data-images="[{'src': '/path/to/image', 'alt': 'The alt text'}]"
+>
+```
--- src/pg-gallery.vue
+++ src/pg-gallery.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="paragraph paragraph--gallery">
-    <slot name="field_media"/>
-    <slot/>
+    <div class="paragraph__title">
+      {{ dataName }}
+    </div>
+    <lupus-slider
+      arrows="1"
+      bullets="1"
+      autoplay="0"
+    >
+      <lupus-slider-slide
+        v-for="image in images"
+      >
+        <img 
+          :alt="image.alt" 
+          :src="image.src"
+        >
+      </lupus-slider-slide>
+    </lupus-slider>
   </div>
 </template>
 <script>
-  export default {
-    name: 'PgGallery',
-  };
+import { LupusSlider, LupusSliderSlide } from 'vue-lupus-slider';
+export default {
+  name: 'PgGallery',
+  components: {
+    'lupus-slider': LupusSlider,
+    'lupus-slider-slide': LupusSliderSlide,
+  },
+  props: {
+    dataName: { type: String, default: () => '' },
+    dataImages: { type: String, default: () => ''}
+  },
+  computed: {
+    images() {
+      return JSON.parse(this.dataImages);
+    },
+  },
+};
 </script>
